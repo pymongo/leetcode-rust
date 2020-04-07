34a8cf399afdd302effa9598f1cd7ab924e65a79
吴翱翔 <pylint@yandex.com>
2020-04-07 21:25:05 +0800

    使用两层遍历暴力解掉第三题

--- src/problem_0003.rs
+++ src/problem_0003.rs
@@ -2,33 +2,28 @@ pub fn run() {
   println!("{}", length_of_longest_substring(String::from("pwwkew")));
 }
 
-use std::collections::BTreeSet;
+pub fn current(s: String) -> i32 {
+  1
+}
 
-fn length_of_longest_substring(s: String) -> i32 {
-  let mut max : usize = 1;
-  let mut uniques : BTreeSet<char> = BTreeSet::new();
+// 164 ms, faster than 12.34%
+// 这个方法太慢了，其实一层遍历就够了
+#[cfg(feature = "unused")]
+fn length_of_longest_substring_slow(s: String) -> i32 {
+  let mut max : usize = 0;
+  let mut uniques : std::collections::BTreeSet<char> = std::collections::BTreeSet::new();
   for (i, char1) in s.chars().enumerate() {
+    uniques.clear();
     uniques.insert(char1);
-    for (j, char2) in s.chars().enumerate() {
-      if j <= i {
-        continue;
-      }
-      println!();
-      print!("({}, {})", i, j);
-      println!("char1 = {}, char2 = {}", char1, char2);
-      println!("uniques.len() = {}", uniques.len());
-      println!();
-      for char in &uniques {
-        print!("{}", *char);
-      }
-      print!("|");
-      print!("i = {}, char1 = {}", i, char1);
-      if !uniques.contains(&char2) {
+    for char2 in s[i+1..].chars() {
+      if uniques.contains(&char2) {
+        // 保证连续不重复
+        break;
+      } else {
         uniques.insert(char2);
       }
     }
     max = std::cmp::max(max, uniques.len());
-    uniques.clear();
   }
   max as i32
 }
