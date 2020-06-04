08ce70096d391423d1d2f630e65ed57a19241088
吴翱翔 <pylint@yandex.com>
2020-06-04 11:44:20 +0800

    添加了斐波那契数列的logn解法的描述

--- src/classic/tail_recursion_fibonacci.rs
+++ src/classic/tail_recursion_fibonacci.rs
@@ -19,12 +19,39 @@ fn tail_recursion_helper(n: i32) -> i32 {
    tail_recursion(n as u32, 1, 1) as i32
 }
 
-/* 耗时0ms，内存2MB，leetcode登顶解法 */
+/* 耗时0ms，内存2MB，时间复杂度O(n)
+还有O(log(n))的解法：
+1. 斐波那契公式(公式中的乘方需要log(n)时间复杂度)
+2. Binet's formula 利用矩阵解斐波那契
+*/
 #[cfg(test)]
 fn tail_recursion(n: u32, a: u32, b: u32) -> u32 {
     if n == 1 {
         b
     } else {
+        // 注意尾递归解法只能从1逼近n，普通递归解法一般是从f(n-1)一直加到f(1)
         tail_recursion(n-1, b, a+b)
     }
+}
+
+#[test]
+fn test_iterate() {
+    for case in &TEST_CASE {
+        dbg!(case);
+        assert_eq!(iterate(case.0), case.1);
+    }
+}
+
+#[cfg(test)]
+fn iterate(n: i32) -> i32 {
+    let (mut a, mut b) = (1u32, 1u32);
+    let mut temp: u32;
+    let mut n = n;
+    while n > 1 {
+        temp = b;
+        b = a + b;
+        a = temp;
+        n -= 1;
+    }
+    b as i32
 }
