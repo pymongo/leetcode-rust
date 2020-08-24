10ac220a6dbabc41cf847bc4c5e1f3f18d7f1b13
吴翱翔 <pylint@yandex.com>
2020-08-24 12:57:31 +0800

    通过原始指针解决了链表反转2的报错，但是仍未解决节点丢失的Bug

--- src/linked_list/reverse_linked_list_2.rs
+++ src/linked_list/reverse_linked_list_2.rs
@@ -7,36 +7,33 @@ use std::boxed::Box;
 struct Solution;
 
 #[cfg(test)]
+type Node = Option<Box<ListNode>>;
+
+#[cfg(test)]
 impl Solution {
     pub fn reverse_range_inplace(head: Option<Box<ListNode>>, m: i32, n: i32) -> Option<Box<ListNode>> {
-        let mut dummy_node = ListNode::new(0);
-        // head is taken to dummy_node.next
-        dummy_node.next = head;
-        let mut dummy = Some(Box::new(dummy_node));
-        let mut node_m_prev = &mut dummy;
-        // use raw pointer
-        let mut ptr = 0 as *mut Box<ListNode>;
-        for _ in 0..m-1 {
-            node_m_prev = &mut node_m_prev.as_mut().unwrap().next;
+        unsafe {
+            let mut dummy = Some(Box::new(ListNode::new(0)));
+            dummy.as_mut().unwrap().next = head;
+            let mut node_m_prev = &mut dummy as *mut Node;
+            for _ in 0..m-1 {
+                node_m_prev = &mut (*node_m_prev).as_mut().unwrap().next as *mut Node;
+            }
+            // rev_head会一直往前挪，但是节点的值和内存地址不变
+            let rev_head = &mut (*node_m_prev).as_mut().unwrap().next as *mut Node;
+            let mut rev_head_next = std::ptr::null_mut();
+            for _ in 0..1 {
+                // 备份rev_head.next
+                rev_head_next = &mut (*rev_head).as_mut().unwrap().next as *mut Node;
+                // rev_head的next指针越过rev_head_next
+                (*rev_head).as_mut().unwrap().next = (*rev_head_next).as_mut().unwrap().next.take();
+                // 将rev_head_next插入到node_m_prev和rev_head之间
+                (*rev_head_next).as_mut().unwrap().next = (*node_m_prev).as_mut().unwrap().next.take();
+                (*node_m_prev).as_mut().unwrap().next = (*rev_head_next).take();
+                // FIXME [1,4,3,2,5]经过一轮后会变成[1,4,2]，丢失节点的原因可能是take导致的，换成mem::swap也一样
+            }
+            return dummy.unwrap().next;
         }
-        let mut rev_head = &mut node_m_prev.as_mut().unwrap().next;
-        for _ in m..n {
-
-            // 备份rev_head.next
-            // if let Some(rev_head_node) = rev_head {
-            //     let rev_head_node_next = rev_head_node.next;
-            //     if let Some(rev_head_node_next_node) = rev_head_node_next {
-            //         rev_head_node.next = rev_head_node_next_node.next;
-            //     }
-            // }
-            // let mut rev_head_next_node = rev_head_node.next.as_mut().unwrap();
-            // // rev_head的next指针越过rev_head_next
-            // rev_head.as_mut().unwrap().next = rev_head_next_node.next;
-            // // 将rev_head_next插入到node_m_prev和rev_head之间
-            // rev_head_next_node.next = node_m_prev_node.next;
-            // node_m_prev_node.next = rev_head_node.next;
-        }
-        return dummy.unwrap().next;
     }
 
     fn vec_to_linked_list(nums: Vec<i32>) -> Option<Box<ListNode>> {
@@ -84,6 +81,15 @@ fn test_traverse_two_list_node() {
     for &(input, m, n, output) in &TEST_CASES {
         let head = arr_to_linked_list(input);
         let output_head = Solution::reverse_between(head, m, n);
+        assert_eq!(linked_list_to_vec(output_head), output.to_vec());
+    }
+}
+
+#[test]
+fn test_reverse_range_inplace() {
+    for &(input, m, n, output) in &TEST_CASES {
+        let head = arr_to_linked_list(input);
+        let output_head = Solution::reverse_range_inplace(head, m, n);
         assert_eq!(linked_list_to_vec(output_head), output.to_vec());
     }
 }
