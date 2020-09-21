705126f50e9d038f0f9abdd19bdfaf3a708e040c
吴翱翔 <pylint@yandex.com>
2020-09-21 09:57:18 +0800

    使用了loop label重构了跳出多层循环的代码

--- src/easy/special_positions_in_a_binary_matrix.rs
+++ src/easy/special_positions_in_a_binary_matrix.rs
@@ -6,32 +6,23 @@ impl Solution {
         let (m, n) = (mat.len(), mat[0].len());
         let mut res = 0;
         for i in 0..m {
-            for j in 0..n {
+            'for_j: for j in 0..n {
                 if mat[i][j] == 1 {
-                    let mut is_match = true;
                     for row in 0..m {
                         if row == i {
                             continue; // for row in 0..m
                         }
                         if mat[row][j] == 1 {
-                            is_match = false;
-                            break; // for row in 0..m
+                            continue 'for_j;
                         }
-                    }
-                    if !is_match {
-                        continue; // for j in 0..n
                     }
                     for col in 0..n {
                         if col == j {
-                            continue; // for col in 0..n
+                            continue;
                         }
                         if mat[i][col] == 1 {
-                            is_match = false;
-                            break; // for col in 0..n
+                            continue 'for_j;
                         }
-                    }
-                    if !is_match {
-                        continue; // for j in 0..n
                     }
                     res += 1;
                 }
